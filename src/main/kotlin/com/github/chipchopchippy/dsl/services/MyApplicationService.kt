dac6cbf7f832bdd422e536d2aef2206b4a52a2fb
GitHub Action <action@github.com>
2020-10-21 11:16:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/chipchopchippy/dsl/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/chipchopchippy/dsl/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.chipchopchippy.dsl.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chipchopchippy.dsl.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/chipchopchippy/dsl/services/MyProjectService.kt
+++ src/main/kotlin/com/github/chipchopchippy/dsl/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.chipchopchippy.dsl.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chipchopchippy.dsl.MyBundle
 
 class MyProjectService(project: Project) {
 
